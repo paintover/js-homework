ecb5876cf552bc1340b68e12ff573d25c02bb803
paintover <tjdus1139@naver.com>
2024-06-22 22:48:52 +0900

    두 번째 과제 : 네이버 로그인 과제

--- mission01/homework/00-homework.js
+++ mission01/homework/00-homework.js
@@ -11,6 +11,31 @@
 
 // }
 
+// 범쌤 답
+// function isObject(data){
+//   return Object.prototype.toString.call(data).slice(8,-1).toLowerCase() === 'object'
+// }
+
+// function getValueAtObject(obj,key){
+
+//   if(typeof key !== 'string'){
+//     throw new TypeError('getValueAtObject 함수의 두 번째 인수는 문자 타입 이어야 합니다.')
+//   }
+  
+//   if(isObject(obj)){
+//     if(Object.prototype.hasOwnProperty.call(obj,key)){
+//       return obj[key];
+//     }
+//     else{
+//       throw new Error(`getValueAtObject 함수의 해당 ${key}가 존재하지 않습니다.`);
+//     }
+//   }
+//   else{
+//     throw new TypeError('getValueAtObject 함수의 첫 번째 인수는 객체 타입 이어야 합니다.');
+//   }
+  
+// }
+
 // // 예시 코드
 // const person = {
 //   name: 'Alice',
@@ -39,6 +64,7 @@ function getNumberAtArray(arr, index) {
 
 }
 
+
 // 예시 코드
 const numbers = [10, 20, 30, 40, 50];
 
@@ -46,6 +72,26 @@ console.log(getNumberAtArray(numbers, 2)); // 30
 console.log(getNumberAtArray(numbers, 4)); // 50
 console.log(getNumberAtArray(numbers, 5)); // Error!
 console.log(getNumberAtArray(numbers, -1)); // Error!
+
+// 범쌤 답
+// 1. arr 변수가 배열인지 확인하기
+// 2. 0보다 크거나 같음 && 배열의 길이보다 작을 때
+
+// function getNumberAtArray(arr, index){
+
+//   if(Array.isArray(arr)){
+//     if(index >= 0 && index < arr.length){
+//       return arr[index];
+//     }
+//     else{
+//       throw new Error('배열에 없는 index입니다.');
+//     }
+//   }
+//   else{
+//     throw new TypeError('getNumberAtArray 함수의 첫 번째 인수는 배열 타입 이어야 합니다.')
+//   }
+  
+// }
 
 // 주어진 인덱스가 배열의 유효한 범위 내에 있는지 확인해야 합니다.
 // 인수로 받은 대상이 배열이 아닐 경우 에러를 생성, 생성한 에러를 반환
--- mission01/naver_login/js/main.js
+++ mission01/naver_login/js/main.js
@@ -13,6 +13,9 @@ const user = {
 
 */
 
+
+// 함수
+
 function emailReg(text){
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
@@ -24,11 +27,68 @@ function pwReg(text){
   return re.test(String(text).toLowerCase());
 }
 
+function getNode(node, context = document) {
+  // if (isString(context)) {
+  //   context = document.querySelector(context);
+  // }
+
+  // context가 document가 아니라면 querySelector를 돌아줘.
+  if (context.nodeType !== 9) context = document.querySelector(context);
+
+  return context.querySelector(node);
+}
+
+getNode('.list'); // ul
+
+function getNodes(node, context = document) {
+  if (context.nodeType !== 9) context = document.querySelector(context);
+
+  return context.querySelectorAll(node);
+}
 
 
+/* 진행과정 */
+
+// 1. email을 정규 표현식에 맞게 입력하였는지 확인
+  const validationId = (node) => {
+    if(typeof node == 'string') node = getNode(node)
+
+    if (emailReg(node.value)) node.classList.remove('is--invalid');
+    else node.classList.add('is--invalid');
+  }
+
+  const validationPw = (node) => {
+    if(typeof node == 'string') node = getNode(node)
+
+    if (pwReg(node.value)) node.classList.remove('is--invalid');
+    else node.classList.add('is--invalid');
+  }
+  
+const userId = getNode('#userEmail')
+
+userId.addEventListener('input',() => validationId(userId))
 
 
+// 2. password를 정규 표현식에 맞게 입력하였는지 확인
 
+const userPw = getNode('#userPassword')
+
+userPw.addEventListener('input',() => validationPw(userPw))
+
+
+// 3 & 4. 로그인 버튼 클릭 시, user.id & user.pw 값이 input 값과 일치하는지 확인
+const LoginButton = getNode('.btn-login')
+
+const handleClickLogin = (e) => {
+  e.preventDefault() 
+
+  if(userId.value === user.id && userPw.value === user.pw) 
+  location.href = "./welcome.html"
+}
+
+// 5. 일치 확인 후, welcome.html 페이지로 이동
+
+LoginButton.addEventListener('click', handleClickLogin)
 
 
 
